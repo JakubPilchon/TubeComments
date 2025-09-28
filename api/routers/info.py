8a9294866bbed2de054bf0b4ba246dab4d7e91f7
JakubPilchon <jakubpilchon@gmail.com>
2025-09-28 20:35:47 +0000

    Added getting most common words endpoint

--- api/routers/info.py
+++ api/routers/info.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 from ..db import SessionDep, Video, Comment, Sentiment
-from sqlmodel import select, func, desc
-
+from ..utils import stopwords
+from sqlmodel import select, func, desc, and_
+from collections import Counter
+import string
 
 info = APIRouter(
     prefix="/info",
-    tags=["info"]
+    tags=["Info"]
 )
 
 @info.get("/video/{id}")
@@ -46,3 +48,40 @@ def get_most_liked_comments(
     result = session.exec(query).mappings().all()
 
     return result
+
+@info.get("/mostCommonWords")
+def get_most_common_word(
+    id: int,
+    sentiment: Sentiment,
+    session: SessionDep,
+    limit: int = 10):
+    """
+    Counters words in every comment of the video with defined sentiment.
+
+    Parameters:
+    - **id** - id of video in database
+    - **sentiment** - sentiment of which comments wil be used in the counting process
+    - **limit** - max number of comment returned
+    """
+
+    word_counter = Counter()
+    query = (
+        select(Comment.commentText)
+        .where(and_(Comment.videoId == id, Comment.sentiment == sentiment))
+    )
+
+    result = session.exec(query)
+    translator = str.maketrans('', '', string.punctuation + '1234567890')
+
+    for text in result:
+        word_counter.update(text
+                             .translate(translator)
+                             .lower()
+                             .split())
+    if result:
+        for stopword in stopwords:
+            word_counter[stopword] = 0
+
+    most_common = {word: count for word, count in word_counter.most_common(limit)}
+
+    return most_common
--- api/utils.py
+++ api/utils.py
@@ -25,6 +25,26 @@ def load_model() -> Pipeline:
     log.info("Model loaded") 
     return pipe
 
+ModelDep = Annotated[Pipeline, Depends(load_model)]
+
+class YoutubeUrl(BaseModel):
+    url: str
+
+    @field_validator("url")
+    @classmethod
+    def validate_youtube(cls, url: str) -> str:
+
+        pattern = r"^(http(s)?://)?(?:(www.|m.)?youtube.com/watch\?v=|youtu.be/)[a-zA-Z0-9\-_]{11}.*$"
+
+        if re.match(pattern, url):
+            return url
+        else:
+            raise ValueError("Provided string is not valid youtube url")
+        
+    @property
+    def video_key(self)-> str:
+        return re.search("(?<=v=)[a-zA-Z0-9\-_]{11}|(?<=youtu\.be/)[a-zA-Z0-9\-_]{11}", self.url).group()
+
 categories = {
     "1": "Film & Animation",
     "2": "Autos & Vehicles",
@@ -60,27 +80,28 @@ categories = {
     "44": "Trailers",
 }
 
-ModelDep = Annotated[Pipeline, Depends(load_model)]
-
-
-class YoutubeUrl(BaseModel):
-    url: str
-
-    @field_validator("url")
-    @classmethod
-    def validate_youtube(cls, url: str) -> str:
-        #pattern = r"""^(http(s)?://)? # protocol
-        #            (?:(www.|m.)?youtube.com/watch\?v=|youtu.be/) # domain
-        #            [a-zA-Z0-9\-_]{11} # video key
-        #            .*$ # other parameters
-        #            """
-        pattern = r"^(http(s)?://)?(?:(www.|m.)?youtube.com/watch\?v=|youtu.be/)[a-zA-Z0-9\-_]{11}.*$"
-
-        if re.match(pattern, url):
-            return url
-        else:
-            raise ValueError("Provided string is not valid youtube url")
-        
-    @property
-    def video_key(self)-> str:
-        return re.search("(?<=v=)[a-zA-Z0-9\-_]{11}|(?<=youtu\.be/)[a-zA-Z0-9\-_]{11}", self.url).group()
+stopwords =  { # chatgpt generated :-)
+    "a","about","above","after","again","against","all","am","an","and","any","are",
+    "arent","as","at","be","because","been","before","being","below","between","both",
+    "but","by","cant","cannot","could","couldnt","did","didnt","do","does","doesnt",
+    "doing","dont","down","during","each","few","for","from","further","had","hadnt",
+    "has","hasnt","have","havent","having","he","hed","hell","hes","her","here",
+    "heres","hers","herself","him","himself","his","how","hows","i","id","ill","im",
+    "ive","if","in","into","is","isnt","it","its","itself","lets","me","more",
+    "most","mustnt","my","myself","no","nor","not","of","off","on","once","only","or",
+    "other","ought","our","ours","ourselves","out","over","own","same","shant","she",
+    "shed","shell","shes","should","shouldnt","so","some","such","than","that",
+    "thats","the","their","theirs","them","themselves","then","there","theres","these",
+    "they","theyd","theyll","theyre","theyve","this","those","through","to","too",
+    "under","until","up","very","was","wasnt","we","wed","well","were","weve",
+    "were","werent","what","whats","when","whens","where","wheres","which","while",
+    "who","whos","whom","why","whys","with","wont","would","wouldnt","you","youd",
+    "youll","youre","youve","your","yours","yourself","yourselves",
+    "also","just","like","one","two","three","us","get","got","may","might","much",
+    "many","still","yet","ever","never","say","says","said","see","seen","go","goes",
+    "went","come","comes","came","make","makes","made","know","known","thing","things",
+    "take","taken","put","puts","want","wanted","use","used","using","back","even",
+    "well","every","everyone","everything","because","however","although","though",
+    "perhaps","rather","quite","often","always","sometimes","already","too",
+    "very","else","such","within","without","across","around","behind","beyond","toward"
+}
